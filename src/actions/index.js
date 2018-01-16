ea3110bacf29c39a74f84e8cddcbaf78e62b79b3
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-01-16 22:33:46 +0100

    connected action creator to component, created container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_WEATHER_KEY = '16dbf7b73f9194d963ad1a85b212e371';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_WEATHER_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_WEATHER_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 
-    const url = `${ROOT_URL}&q=${city},pl`;
+    const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
 
     return {
