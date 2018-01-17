c57f38714a1dfcb5a1bbd7d02bdfa1fe839078c0
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-01-17 22:32:30 +0100

    changed reducer, added new container file

--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,6 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index';
 
-export default function WeatherReducer(state = null, action) {
+export default function WeatherReducer(state = [], action) {
     switch (action.type) {
         case FETCH_WEATHER:
             return [action.payload.data, ...state];
