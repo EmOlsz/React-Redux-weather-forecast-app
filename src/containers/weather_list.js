9e9a19a028465343d6dc844832b516f6db009ccc
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-01-24 22:40:20 +0100

    added new reusable chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,40 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/single_chart';
 
 class WeatherList extends Component {
 
     renderWeather(element) {
+
+        const temperature = element.list.map(weather => weather.main.temp);
+        const pressure = element.list.map(weather => weather.main.pressure);
+        const humidity = element.list.map(weather => weather.main.humidity);
+
         return (
             <tr key={element.city.id}>
                 <td>{element.city.name}</td>
-                <td>
-                    <Sparklines
-                        height={120}
-                        width={180}
-                        data={element.list.map(weather => weather.main.temp)}
-                    >
-                        <SparklinesLine color="rgba(255, 0, 0, .3)" />
-                    </Sparklines>
-                </td>
-                <td>
-                    <Sparklines
-                        height={120}
-                        width={180}
-                        data={element.list.map(weather => weather.main.pressure)}
-                    >
-                        <SparklinesLine color="rgba(0, 255, 0, .3)" />
-                    </Sparklines>
-                </td>
-                <td>
-                    <Sparklines
-                        height={120}
-                        width={180}
-                        data={element.list.map(weather => weather.main.humidity)}
-                    >
-                        <SparklinesLine color="rgba(0, 0, 255, .3)" />
-                    </Sparklines>
-                </td>
+                <td><Chart data={temperature} color={"rgba(0, 0, 0, .5)"} /></td>
+                <td><Chart data={pressure} color={"rgba(255, 0, 0, .5)"} /></td>
+                <td><Chart data={humidity} color={"rgba(0, 0, 255, .5)"} /></td>
             </tr>
         );
     }
